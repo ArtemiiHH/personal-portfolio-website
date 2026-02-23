1f59d9de7812d4e48ac6ee8b58f259cfbb1db982
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 20:50:48 +0200

    Switch hero code with about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,5 +1,32 @@
+import styles from "./About.module.css";
+
 export default function About() {
   return (
-    <section id="about"></section>
+    <div className={styles.outerContainer}>
+      <section id="about" className={styles.about}>
+        {/* Left Column */}
+        <div className={styles.left}>
+          {/* Title */}
+          <h1>My name is...</h1>
+          {/* Description */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu
+            sagittis mi. Nulla rhoncus efficitur dolor, vitae consequat ante
+            feugiat at. Sed maximus bibendum vehicula. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Donec nec nunc eu neque
+            aliquam ullamcorper.{" "}
+          </p>
+          {/* Button Group */}
+          <div className={styles.buttonGroup}>
+            <button>View work</button>
+            <button>Download CV</button>
+          </div>
+        </div>
+        {/* Right Column */}
+        <div className={styles.right}>
+          <div className={styles.imageFrame}></div>
+        </div>
+      </section>
+    </div>
   );
 }
--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -3,30 +3,7 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.outerContainer}>
-      <section id="hero" className={styles.hero}>
-        {/* Left Column */}
-        <div className={styles.left}>
-          {/* Title */}
-          <h1>My name is...</h1>
-          {/* Description */}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu
-            sagittis mi. Nulla rhoncus efficitur dolor, vitae consequat ante
-            feugiat at. Sed maximus bibendum vehicula. Interdum et malesuada
-            fames ac ante ipsum primis in faucibus. Donec nec nunc eu neque
-            aliquam ullamcorper.{" "}
-          </p>
-          {/* Button Group */}
-          <div className={styles.buttonGroup}>
-            <button>View work</button>
-            <button>Download CV</button>
-          </div>
-        </div>
-        {/* Right Column */}
-        <div className={styles.right}>
-          <div className={styles.imageFrame}></div>
-        </div>
-      </section>
+      <section id="hero" className={styles.hero}></section>
     </div>
   );
 }
