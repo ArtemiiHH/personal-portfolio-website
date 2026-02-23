df151f9523c3b1d12ffc7f2d4f83c28e0a7add95
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 17:12:32 +0200

    Add projects + style

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,3 +1,35 @@
+import styles from "./Projects.module.css";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  return (
+    <section className={styles.projects}>
+      {/* Title */}
+      <h2>Projects</h2>
+
+      {/* Card Grid */}
+      <ul className={styles.grid}>
+        {/* Each Project Card */}
+        <li className={styles.projectCard}>
+          <h3>Project</h3>
+          <p>Description...</p>
+        </li>
+        <li className={styles.projectCard}>
+          <h3>Project</h3>
+          <p>Description...</p>
+        </li>
+        <li className={styles.projectCard}>
+          <h3>Project</h3>
+          <p>Description...</p>
+        </li>
+        <li className={styles.projectCard}>
+          <h3>Project</h3>
+          <p>Description...</p>
+        </li>
+        <li className={styles.projectCard}>
+          <h3>Project</h3>
+          <p>Description...</p>
+        </li>
+      </ul>
+    </section>
+  );
 }
