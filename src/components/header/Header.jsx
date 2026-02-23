7bd05237b30caa3af5f1aa46e0b1110ff3cabbc7
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 15:11:06 +0200

    Give header basic styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,9 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className={styles.header}>
+      <h1>Header</h1>
+    </header>
+  );
 }
