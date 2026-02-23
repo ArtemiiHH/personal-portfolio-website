0b3c9d9ccdbffefe20932fcdf0ee2ae20140c4bd
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 14:59:15 +0200

    Import/export all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+// Import components
+import Header from "./components/header/Header";
+import Hero from "./sections/hero/Hero";
+import About from "./sections/about/About";
+import Skills from "./sections/skills/Skills";
+import Projects from "./sections/projects/Projects";
+import Contact from "./sections/contact/Contact";
+import Footer from "./components/footer/Footer";
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
+    </>
+  );
+}
 
 export default App;
