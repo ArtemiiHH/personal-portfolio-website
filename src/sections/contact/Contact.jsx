7164925fe956bbeef54368b0c96ef2a49d7ca396
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 20:05:19 +0200

    Blackout bottom borders

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,5 +1,11 @@
+import styles from "./Contact.module.css";
+
 export default function Contact() {
   return (
-    <section id="contact"></section>
+    <div className={styles.outerContainer}>
+      <section id="contact" className={styles.contact}>
+        <h1>Let's work together!</h1>
+      </section>
+    </div>
   );
 }
