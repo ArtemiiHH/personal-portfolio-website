122cac96eafe0a57ff4ba816f21431a4009e32fd
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 22:04:46 +0200

    Style title, undertext, buttons

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -6,14 +6,14 @@ export default function Hero() {
     <div className={styles.outerContainer}>
       <section id="hero" className={styles.hero}>
         {/* Title */}
-        <h1>Full Stack Developer</h1>
-        <h2>Artemii Barashkov</h2>
+        <h1 className={styles.title}>Full Stack Developer</h1>
+        <h2 className={styles.undertext}>Artemii Barashkov</h2>
         {/* Button Group */}
         <div className={styles.buttonGroup}>
           <button className={styles.workButton}>
             View work <img src={arrowSvg} />
           </button>
-          <button>Download CV</button>
+          <button className={styles.cvButton}>Download CV</button>
         </div>
       </section>
     </div>
