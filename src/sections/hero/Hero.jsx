7e2fcb2b351164c0ec638effa4dc82c65c961073
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 21:30:55 +0200

    Import arrow svg

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,9 +1,21 @@
 import styles from "./Hero.module.css";
+import arrowSvg from "../../assets/arrow.svg";
 
 export default function Hero() {
   return (
     <div className={styles.outerContainer}>
-      <section id="hero" className={styles.hero}></section>
+      <section id="hero" className={styles.hero}>
+        {/* Title */}
+        <h1>Full Stack Developer</h1>
+        <h2>Artemii Barashkov</h2>
+        {/* Button Group */}
+        <div className={styles.buttonGroup}>
+          <button className={styles.workButton}>
+            View work <img src={arrowSvg} />
+          </button>
+          <button>Download CV</button>
+        </div>
+      </section>
     </div>
   );
 }
