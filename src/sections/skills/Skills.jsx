944f63fb954aafa068ae853bdb676997139975cc
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 16:37:51 +0200

    Add content + style skills

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,3 +1,19 @@
+import styles from "./Skills.module.css";
+
 export default function Skills() {
-  return <h1>Skills</h1>;
+  return (
+    <section className={styles.skills}>
+      {/* Title */}
+      <h2>My stack</h2>
+
+      {/* Stack List */}
+      <ul>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    </section>
+  );
 }
