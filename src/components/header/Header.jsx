2d0a36a5a2a6eec0154270f78c246980bd7ad736
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-23 15:36:56 +0200

    Style header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,19 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      {/* Name/Logo */}
+      <h1>
+        <a href="" className={styles.name}>Artemii</a>
+      </h1>
+      {/* Navigation Tab */}
+      <nav>
+        <ul className={styles.navbar}>
+          <li>About</li>
+          <li>Work</li>
+          <li>Skills</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
