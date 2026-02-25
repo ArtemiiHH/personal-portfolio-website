32169baed89b0cc6a1047387b7dd77695b40b440
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-25 17:01:37 +0200

    Add title to projects + change sql svg

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -3,40 +3,42 @@ import projectImage from "../../assets/project-image.png";
 
 export default function Projects() {
   return (
-    <div className={styles.outerContainer}>
-      <section id="projects" className={styles.projects}>
-        {/* Title */}
-        <h2>Projects</h2>
-
-        {/* Card Grid */}
-        <ul className={styles.grid}>
-          {/* Each Project Card */}
-          <li className={styles.projectCard}>
-            <img src={projectImage} className={styles.projectImage} />
-            <h3>Project</h3>
-            <p className={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              finibus, felis a elementum lacinia, tellus massa vehicula leo, id
-              maximus urna ipsum non metus.
-            </p>
-          </li>
-          <li className={styles.projectCard}>
-            <img src={projectImage} className={styles.projectImage} />
-            <h3>Project</h3>
-            <p>Description...</p>
-          </li>
-          <li className={styles.projectCard}>
-            <img src={projectImage} className={styles.projectImage} />
-            <h3>Project</h3>
-            <p>Description...</p>
-          </li>
-          <li className={styles.projectCard}>
-            <img src={projectImage} className={styles.projectImage} />
-            <h3>Project</h3>
-            <p>Description...</p>
-          </li>
-        </ul>
-      </section>
-    </div>
+    <section id="projects">
+      <div className={styles.projectsTitle}>
+        <h1>Projects</h1>
+      </div>
+      <div className={styles.outerContainer}>
+        <section className={styles.projects}>
+          {/* Card Grid */}
+          <ul className={styles.grid}>
+            {/* Each Project Card */}
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <h3>Project</h3>
+              <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                finibus, felis a elementum lacinia, tellus massa vehicula leo,
+                id maximus urna ipsum non metus.
+              </p>
+            </li>
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <h3>Project</h3>
+              <p>Description...</p>
+            </li>
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <h3>Project</h3>
+              <p>Description...</p>
+            </li>
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <h3>Project</h3>
+              <p>Description...</p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </section>
   );
 }
