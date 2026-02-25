702d1bf93680137b17cc704900e2f1157a717581
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-25 20:27:56 +0200

    Add title attribute to buttons in projects

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -25,6 +25,83 @@ export default function Projects() {
                 </p>
                 <div className={styles.group}>
                   <div className={styles.iconGroup}>
+                    <a
+                      href="https://geography-memo-game.netlify.app/"
+                      target="_blank"
+                      title="Live preview"
+                    >
+                      <img src={previewIcon} className={styles.icon} />
+                    </a>
+                    <a
+                      href="https://github.com/ArtemiiHH/memory-card-game"
+                      target="_blank"
+                      title="GitHub Link"
+                    >
+                      <img src={githubLogo} className={styles.icon} />
+                    </a>
+                  </div>
+                  <p className={styles.stackLabel}>React</p>
+                </div>
+              </div>
+            </li>
+
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <div className={styles.textBox}>
+                <h3>Geography Game</h3>
+                <p className={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                  finibus, felis a elementum lacinia, tellus massa vehicula leo,
+                  id maximus urna ipsum non metus.
+                </p>
+                <div className={styles.group}>
+                  <div className={styles.iconGroup}>
+                    <a href="">
+                      <img src={previewIcon} className={styles.icon} />
+                    </a>
+                    <a href="">
+                      <img src={githubLogo} className={styles.icon} />
+                    </a>
+                  </div>
+                  <p className={styles.stackLabel}>React</p>
+                </div>
+              </div>
+            </li>
+
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <div className={styles.textBox}>
+                <h3>Geography Game</h3>
+                <p className={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                  finibus, felis a elementum lacinia, tellus massa vehicula leo,
+                  id maximus urna ipsum non metus.
+                </p>
+                <div className={styles.group}>
+                  <div className={styles.iconGroup}>
+                    <a href="">
+                      <img src={previewIcon} className={styles.icon} />
+                    </a>
+                    <a href="">
+                      <img src={githubLogo} className={styles.icon} />
+                    </a>
+                  </div>
+                  <p className={styles.stackLabel}>React</p>
+                </div>
+              </div>
+            </li>
+
+            <li className={styles.projectCard}>
+              <img src={projectImage} className={styles.projectImage} />
+              <div className={styles.textBox}>
+                <h3>Geography Game</h3>
+                <p className={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                  finibus, felis a elementum lacinia, tellus massa vehicula leo,
+                  id maximus urna ipsum non metus.
+                </p>
+                <div className={styles.group}>
+                  <div className={styles.iconGroup}>
                     <a href="">
                       <img src={previewIcon} className={styles.icon} />
                     </a>
