85584b3caf5b79f9b9b14f60b2b344498329c947
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-06-29 14:18:49 +0300

    Add tint to project cards

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from "./Projects.module.css";
 import projectImage from "../../assets/project-image.png";
-import projectPortfolio from "../../assets/project-portfolio.jpg";
-import projectConsulting from "../../assets/consulting-leads-mockup2.png";
+import casePilot from "../../assets/case-pilot-mockup8.png";
+import consultingLeads from "../../assets/consulting-leads-mockup2.png";
 import previewIcon from "../../assets/preview.svg";
 import githubLogo from "../../assets/github-logo.svg";
 
@@ -16,8 +16,47 @@ export default function Projects() {
           {/* Card Grid */}
           <ul className={styles.grid}>
             {/* Each Project Card */}
+
             <li className={styles.projectCard}>
-              <img src={projectConsulting} className={styles.projectImage} />
+              <a href="https://getcasepilot.co/" target="_blank">
+                <img src={casePilot} className={styles.projectImage} />
+              </a>
+              <div className={styles.textBox}>
+                <h3>Email Automation software for law firms</h3>
+                <p className={styles.text}>
+                  Personal portfolio website you are viewing now.
+                </p>
+                <div className={styles.group}>
+                  <div className={styles.iconGroup}>
+                    <a
+                      href="https://artemii-barashkov.netlify.app/"
+                      target="_blank"
+                      title="Live preview"
+                    >
+                      <img src={previewIcon} className={styles.icon} />
+                    </a>
+                    <a
+                      href="https://github.com/ArtemiiHH/personal-portfolio-website"
+                      target="_blank"
+                      title="GitHub Link"
+                    >
+                      <img src={githubLogo} className={styles.icon} />
+                    </a>
+                  </div>
+                  <div className={styles.tags}>
+                    <p className={styles.stackLabel}>React</p>
+                    <p className={styles.stackLabel}>Node</p>
+                    <p className={styles.stackLabel}>Express</p>
+                    <p className={styles.stackLabel}>PostgreSQL</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+            <li className={styles.projectCard}>
+              <a href="https://getconsultingleads.com/" target="_blank">
+                <img src={consultingLeads} className={styles.projectImage} />
+              </a>
               <div className={styles.textBox}>
                 <h3>A B2B tool that helps consulting firms find leads</h3>
                 <p className={styles.text}>
@@ -63,35 +102,6 @@ export default function Projects() {
                     </a>
                     <a
                       href="https://github.com/ArtemiiHH/memory-card-game"
-                      target="_blank"
-                      title="GitHub Link"
-                    >
-                      <img src={githubLogo} className={styles.icon} />
-                    </a>
-                  </div>
-                  <p className={styles.stackLabel}>React</p>
-                </div>
-              </div>
-            </li>
-
-            <li className={styles.projectCard}>
-              <img src={projectPortfolio} className={styles.projectImage} />
-              <div className={styles.textBox}>
-                <h3>Personal Portfolio</h3>
-                <p className={styles.text}>
-                  Personal portfolio website you are viewing now.
-                </p>
-                <div className={styles.group}>
-                  <div className={styles.iconGroup}>
-                    <a
-                      href="https://artemii-barashkov.netlify.app/"
-                      target="_blank"
-                      title="Live preview"
-                    >
-                      <img src={previewIcon} className={styles.icon} />
-                    </a>
-                    <a
-                      href="https://github.com/ArtemiiHH/personal-portfolio-website"
                       target="_blank"
                       title="GitHub Link"
                     >
